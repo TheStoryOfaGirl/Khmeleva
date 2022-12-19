facda814747f561aa9d91b7060847c1fb62dce4a
TheStoryOfaGirl <Viktoria.Khmeleva@urfu.me>
2022-12-19 22:30:09 +0500

    Задание 3.3.1

--- Task323.py
+++ Task323.py
@@ -29,8 +29,8 @@ def get_statistic_by_city(file):
 
 
 if __name__ == '__main__':
-    file = input('Название файла:')
-    vacancy = input('Название вакансии:')
+    file = input('Название файла: ')
+    vacancy = input('Название вакансии: ')
     df = pd.read_csv(file)
     df["years"] = df["published_at"].apply(lambda s: datetime.strptime(s, "%Y-%m-%dT%H:%M:%S%z").year)
     years = df["years"].unique()
