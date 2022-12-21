c177b43bf57b3beb4ad414efac251d92a8449285
TheStoryOfaGirl <Viktoria.Khmeleva@urfu.me>
2022-12-21 17:36:45 +0500

    Задание 3.3.2

--- Task331.py
+++ Task331.py
@@ -14,12 +14,7 @@ late_date = list(df['published_at'])[-1]
 data_currency = pd.DataFrame(columns=["date", "BYR", "USD", "EUR", "KZT", "UAH"])
 for year in range(2003, 2023):
     for month in range(1, 13):
-        if month == 1 and year == 2003:
-            date = '24/01/2003'
-        elif month == 7 and year == 2022:
-            date = '19/07/2022'
-        else:
-            date = f'01/0{month}/{year}' if month < 10 else f'01/{month}/{year}'
+        date = f'01/0{month}/{year}' if month < 10 else f'01/{month}/{year}'
         res = f'http://www.cbr.ru/scripts/XML_daily.asp?date_req={date}'
         new_row = {'date': date}
         values_cur = pd.read_xml(res, encoding='cp1251')
@@ -28,7 +23,7 @@ for year in range(2003, 2023):
                 value = '.'.join(str(values_cur[values_cur['CharCode'] == cur]['Value'].values[0]).split(','))
                 new_row[cur] = float(value) / int(values_cur[values_cur['CharCode'] == cur]['Nominal'].values[0])
         data_currency = pd.concat([data_currency, pd.DataFrame.from_records([new_row])], axis=0, ignore_index=True)
-        if date == '19/07/2022':
+        if date == '01/07/2022':
             break
 data_currency.to_csv("data_currency.csv", index=False)
 print(data_currency)
---
+++ Task332.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import math
+
+file = 'vacancies_dif_currencies.csv'
+df = pd.read_csv(file)
+currencies = pd.read_csv('data_currency.csv')
+df.insert(1, 'salary', None)
+for row in df.itertuples():
+    salary_from = row.salary_from
+    salary_to = row.salary_to
+    salary_currency = row.salary_currency
+    salary = row.salary
+    if type(salary_currency) is str:
+        if not math.isnan(salary_from) and not math.isnan(salary_to):
+            salary = (salary_from + salary_to) / 2
+        elif not math.isnan(salary_from):
+            salary = salary_from
+        elif not math.isnan(salary_to):
+            salary = salary_to
+        if salary_currency != 'RUR' and salary_currency in ["BYR", "USD", "EUR", "KZT", "UAH"]:
+            ratio_cur = currencies[currencies['date']
+                                   == f'01/{row.published_at[5:7]}/{row.published_at[:4]}'][salary_currency].values[0]
+            salary = salary * ratio_cur if not math.isnan(ratio_cur) else None
+        elif salary_currency != 'RUR':
+            salary = None
+        df.at[df.index[int(row.Index)], 'salary'] = salary
+    print(df.index[int(row.Index)])
+df.pop('salary_from')
+df.pop('salary_to')
+df.pop('salary_currency')
+df.to_csv("vacancies_result.csv", index=False)
